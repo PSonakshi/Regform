21bbcff25dc4752648c9d7ad88d2413f49672830
PSonakshi <158698040+PSonakshi@users.noreply.github.com>
2024-08-26 23:16:32 +0530

    third commit

--- src/firebase.js
+++ src/firebase.js
@@ -57,5 +57,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
+export const auth = getAuth(app);
 export default app;
